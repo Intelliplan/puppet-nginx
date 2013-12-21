e6554069bbc97acaaac874c5964c648dd81a3b8d
Henrik <henrik@haf.se>
2013-12-21 16:34:27 +0100

    whitespace and style formatting

--- manifests/disable_site.pp
+++ manifests/disable_site.pp
@@ -4,15 +4,13 @@
 # This definition is private, not intended to be called directly
 #
 define nginx::disable_site {
-
   file {
     "/etc/nginx/sites-enabled/${name}.conf":
-      ensure => absent,
+      ensure  => absent,
       notify  => Service['nginx'],
       require => Package['nginx'];
     "/etc/nginx/sites-available/${name}.conf":
       ensure  => absent,
       require => File["/etc/nginx/sites-enabled/${name}.conf"];
   }
-
-} # end nginx::remove_site() 
+}
