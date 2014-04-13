496baaf9c4c11cf9ac15a87fac54cdae969792e2
Henrik <henrik@haf.se>
2014-04-13 19:44:05 +0200

    moving in params into init

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,34 +15,28 @@
 #   - nginx.conf.erb => /etc/nginx/nginx.conf
 #
 class nginx(
-  $user               = $nginx::params::user,
-  $group              = $nginx::params::group,
-  $worker_processes   = $nginx::params::worker_processes, 
-  $worker_connections = $nginx::params::worker_connections,
-  $includes_dir       = $nginx::params::includes_dir,
-  $conf               = $nginx::params::conf,
-  $etc_dir            = $nginx::params::etc_dir,
-  $log_dir            = $nginx::params::log_dir,
-  $proxy_params       = $nginx::params::proxy_params,
-  $data_dir           = $nginx::params::data_dir,
-  $sites_enabled      = $nginx::params::sites_enabled,
-  $sites_available    = $nginx::params::sites_available
-) inherits nginx::params {
-
-  anchor { 'nginx::start': }
-  anchor { 'nginx::end': }
+  $user               = 'nginx',
+  $group              = 'nginx',
+  $worker_processes   = '1',
+  $worker_connections = '1024',
+  $etc_dir            = '/etc/nginx',
+  $log_dir            = '/var/log/nginx',
+  $data_dir           = '/var/www',
+  $pid_file           = '/var/run/nginx.pid',
+  $includes_dir       = "${etc_dir}/includes",
+  $conf               = "${etc_dir}/conf.d",
+  $proxy_params       = "${includes_dir}/proxy_params",
+  $sites_enabled      = "${etc_dir}/sites-enabled",
+  $sites_available    = "${etc_dir}/sites-available"
+) {
 
   package { 'nginx': 
     ensure  => latest, # http://nginx.org/en/security_advisories.html
-    require => Anchor['nginx::start'],
-    before  => Anchor['nginx::end'],
   }
 
   group { $group:
     ensure  => present,
     system  => true,
-    require => Anchor['nginx::start'],
-    before  => Anchor['nginx::end'],
   }
 
   user { $user:
@@ -60,110 +54,55 @@ class nginx(
     enable     => true,
     hasrestart => true,
     restart    => '/etc/init.d/nginx reload',
-    require    => [
-      Anchor['nginx::start'],
-      File["${etc_dir}/nginx.conf"],
-    ],
-    before     => Anchor['nginx::end'],
+    require    => File["${etc_dir}/nginx.conf"],
+  }
+
+  File {
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nginx'],
   }
 
   file {
     "${etc_dir}/nginx.conf":
       ensure  => present,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
       content => template('nginx/nginx.conf.erb'),
-      notify  => Service['nginx'],
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      notify  => Service['nginx'];
 
     $conf:
       ensure  => absent,
-      force   => true,
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      force   => true;
 
     "${etc_dir}/ssl":
-      ensure  => directory,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      ensure  => directory;
 
     $includes_dir:
-      ensure  => directory,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      ensure  => directory;
 
     "${etc_dir}/fastcgi_params":
-      ensure  => absent,
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      ensure  => absent;
 
     $proxy_params:
-      ensure  => present,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
       content => template('nginx/includes/proxy_params'),
-      notify  => Service['nginx'],
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      notify  => Service['nginx'];
 
     $data_dir:
       ensure  => directory,
       mode    => '0644',
       owner   => $user,
-      group   => $group,
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+      group   => $group;
 
-    $log_dir: 
+    $log_dir:
       ensure  => directory,
       mode    => '0640',
-      owner   => 'root',
-      group   => 'root',
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start']
-      ],
-      before  => Anchor['nginx::end'];
+      require => Package['nginx'];
 
-    [ $sites_available , $sites_enabled ]:
-      ensure => directory,
-      mode => '0644',
-      owner => 'root',
-      group => 'root',
-      require => [
-        Package['nginx'],
-        Anchor['nginx::start'],
-      ],
-      before => Anchor['nginx::end'];
+    $sites_available:
+      ensure => directory;
+
+    $sites_enabled:
+      ensure => directory;
   } # end litany of file resources
 } # end init.pp
